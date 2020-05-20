e9bd1f6b99503e9a0c7413486b0fb9b346d30427
Dams2 <damien-rojo@hotmail.fr>
2020-05-20 18:52:31 +0200

    V1

---
+++ ChimereTests/ChimereTests.swift
@@ -0,0 +1,14 @@
+//
+//  ChimereTests.swift
+//  ChimereTests
+//
+//  Created by Damien Rojo on 20.05.20.
+//  Copyright © 2020 Damien Rojo. All rights reserved.
+//
+
+import XCTest
+@testable import Chimere
+
+class ChimereUITests: XCTestCase {
+
+}
